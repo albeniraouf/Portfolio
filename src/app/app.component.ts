a2361bfec3cb8dad457a76bf14e377475a09bbf8
Raouf Albeni <raouf.a@socienta.com>
2022-10-10 00:11:00 +0300

    fixed meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
 
 import { BehaviorSubject } from 'rxjs';
 import { ClockService } from './services/clock.service';
@@ -10,21 +9,15 @@ import { ClockService } from './services/clock.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'Raouf Albeni';
+  title = `Raouf Albeni's Portfolio`;
   dark = new BehaviorSubject(true);
 
-  constructor(clockService: ClockService, meta: Meta) {
+  constructor(clockService: ClockService) {
     clockService.time.subscribe((time) => {
       const curHr = time.getHours();
       if (curHr < 6 || curHr >= 18) this.dark.next(true);
       else this.dark.next(false);
     });
-    meta.addTags([
-      { name: 'og:title', content: `Raouf Albeni's Portfolio` },
-      { name: 'og:image', content: 'https://albeniraouf.github.io/Portfolio/assets/bitmojies/hi.png' },
-      { name: 'og:url', content: 'https://albeniraouf.github.io/Portfolio' },
-      { name: 'og:type', content: 'website' },
-    ]);
   }
   ngOnInit() {}
 }
