3b252dfcfec90f80e458f89b7cf7ee1d89f48bf3
Raouf Albeni <raouf.albeni@gmail.com>
2025-05-05 21:59:30 +0400

    delete old version

--- src/app/utils/compare.ts
+++
@@ -1,9 +0,0 @@
-export const compare = <T>(a: T, b: T, desc?: boolean) => {
-  if (a > b) return desc ? -1 : 1;
-  if (a < b) return desc ? 1 : -1;
-  return 0;
-};
-
-export const compareBy = <T>(a: T, b: T, key: keyof T, desc?: boolean) => {
-  return compare(a[key], b[key], desc);
-};
