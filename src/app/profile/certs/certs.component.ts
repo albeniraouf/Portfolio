662c9760616ec097603695fd5d6af92d0151931d
Raouf Albeni <raouf.albeni@gmail.com>
2025-05-12 12:01:53 +0400

    added popup for cert display

--- src/app/profile/certs/certs.component.ts
+++ src/app/profile/certs/certs.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 import { Certificate } from '../profile.models';
+import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import gsap from "gsap"
+
 @Component({
   selector: 'app-certs',
   standalone: false,
-  templateUrl: './certs.component.html'
+  templateUrl: './certs.component.html',
 })
-export class CertsComponent {
-  @Input() certs!: Certificate[]
+export class CertsComponent implements AfterViewInit {
+  @Input() certs!: Certificate[];
+  showModal: boolean = false;
+  selectedImage: string = '';
 
   ngAfterViewInit() {
     gsap.registerPlugin(ScrollTrigger);
@@ -23,7 +26,8 @@ export class CertsComponent {
         toggleActions: 'play none none reset',
       },
     });
-    gsap.utils.toArray('.certificate-card').forEach((card: any) => {
+
+    gsap.utils.toArray('.certification-card').forEach((card: any) => {
       gsap.from(card, {
         opacity: 0,
         scale: 0.9,
@@ -35,6 +39,50 @@ export class CertsComponent {
           toggleActions: 'play none none reset',
         },
       });
+    });
+
+    gsap.set('.modal-overlay', { opacity: 0, display: 'none' });
+    gsap.set('.modal-content', { scale: 0.8, opacity: 0 });
+  }
+
+  openModal(image: string) {
+    this.selectedImage = image;
+    this.showModal = true;
+
+    gsap.to('.modal-overlay', {
+      opacity: 1,
+      display: 'block',
+      duration: 0.3,
+      ease: 'power2.out',
+      onComplete: () => {
+        gsap.to('.modal-content', {
+          scale: 1,
+          opacity: 1,
+          duration: 0.5,
+          ease: 'back.out(1.7)',
+        });
+      },
+    });
+  }
+
+  closeModal() {
+    gsap.to('.modal-content', {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.3,
+      ease: 'power2.in',
+      onComplete: () => {
+        gsap.to('.modal-overlay', {
+          opacity: 0,
+          display: 'none',
+          duration: 0.2,
+          ease: 'power2.in',
+          onComplete: () => {
+            this.showModal = false;
+            this.selectedImage = '';
+          },
+        });
+      },
     });
   }
 }
